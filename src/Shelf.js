1557220f26c09c7538d351fbdfd030ec5805da9c
Ioan Zicu <ioan.zicu94@gmail.com>
2019-04-04 15:58:56 +0200

    Add Shelf Component and Array with Book Objects for each Shelf

---
+++ src/Shelf.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class Shelf extends Component {
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.booksList[0].shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+
+            {this.props.booksList[0].books.map((book) =>
+              <li key={book.author}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={book.style}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.author}</div>
+                </div>
+              </li>
+            )}
+
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
